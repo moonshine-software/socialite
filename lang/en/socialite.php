bd135d941cbd204aab964d8d9ac6d5c21dc5f50a
Ichi <ichi-perm@yandex.ru>
2025-01-29 08:49:57 +0500

    add key to lang

--- lang/en/socialite.php
+++ lang/en/socialite.php
@@ -3,5 +3,6 @@
 return [
     'link_exists' => 'Account already exists',
     'link_success' => 'Account linked',
+    'link_socialite' => 'Link account',
     'linked_socialite' => 'Account linked',
 ];
